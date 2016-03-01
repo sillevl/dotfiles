83026d852377dd534c057e2b11674a5c817f913b
Sille Van Landschoot <info@sillevl.be>
2016-03-01 10:07:35 +0100

    Create prompt.ps1

---
+++ powershell/prompt.ps1
@@ -0,0 +1,25 @@
+function prompt { 
+   # our theme 
+   $cdelim = [ConsoleColor]::DarkCyan 
+   $chost = [ConsoleColor]::Green 
+   $cloc = [ConsoleColor]::Cyan 
+
+   write-host "$([char]0x0A7) " -n -f $cloc 
+   #write-host ([net.dns]::GetHostName()) -n -f $chost 
+   write-host '{' -n -f $cdelim 
+   write-host (shorten-path (pwd).Path) -n -f $cloc 
+   write-host '}' -n -f $cdelim 
+
+   Write-VcsStatus
+
+   return '> ' 
+}
+
+function shorten-path([string] $path) { 
+   $loc = $path.Replace($HOME, '~') 
+   # remove prefix for UNC paths 
+   $loc = $loc -replace '^[^:]+::', '' 
+   # make path shorter like tabs in Vim, 
+   # handle paths starting with \\ and . correctly 
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
+}
