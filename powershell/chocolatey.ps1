04bed9f875e32fd8c6a8d93a9ea3ad7c0972b693
Sille Van Landschoot <info@sillevl.be>
2018-10-12 14:58:56 +0200

    update choco software installation list

--- powershell/chocolatey.ps1
+++ powershell/chocolatey.ps1
@@ -3,15 +3,38 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 
 # install applications
 
-choco install -y                                          ^
-googlechrome                                              ^
-nodejs.install                                            ^
- git -params "/GitAndUnixToolsOnPath"                     ^
-atom                                                      ^
-#ruby                                                     ^
-railsinstaller                                            ^
-cmdermini                                                 ^
-putty                                                     ^
-vim                                                       ^
-curl                                                      ^
-poshgit                                                   ^
+choco install 7zip  -y
+choco install putty -y
+choco install nodejs -y
+choco install git --params"/GitAndUnixToolsOnPath /NoShellIntegration"  -y
+choco install openssh  -y
+choco install php  -y
+choco install ruby  -y
+choco install msys2 -y 
+choco install vscode  -y
+choco install golang  -y
+choco install rust -y
+choco install spotify -y
+choco install sonos-controller -y
+choco install hyper -y
+choco install yarn -y
+choco install python -y
+choco install python2 -y
+choco install hg -y
+choco install composer -y
+choco install virtualbox -y
+choco install docker-toolbox -y
+choco install slack -y
+choco install wireshark -y
+choco install winscp -y
+choco install arduino  -y
+choco install openvpn  -y
+choco install postman -y
+choco install etcher -y
+choco install poshgit -y
+choco install steam -y
+choco install vivaldi -y
+choco install gcc-arm-embedded -y
+choco install jdk10 -y
+choco install obs-studio -y
+choco install pushbullet-chrome -y
